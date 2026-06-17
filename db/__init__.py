1860cb20c839c78659e9abb362674685a9ed1bcd
Nik3719 <Qzmpqzmp35@yandex.ru>
2026-06-18 03:48:48 +0500

    переработка импортов через __init__

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,10 @@
-
+from .database import (
+    init_db,
+    search_user,
+    add_user,
+    is_email_registered,
+    is_phone_registered,
+    get_chat_history,
+    add_chat_message,
+    clear_chat_history,
+)
